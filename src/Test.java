16da64cfadf5ef0a82a525493a50b27f3f402604
xumenger <xumenger@126.com>
2018-11-21 23:23:29 +0800

    test case

---
+++ src/Test.java
@@ -0,0 +1,36 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Observable.create(new Observable.OnSubscribe<Integer>() 
+        {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    subscriber.onNext(i);
+                }
+            }
+        }).subscribe(new Subscriber<Integer>() 
+        {
+            @Override
+            public void onCompleted()
+            {
+                System.out.println("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable t)
+            {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onNext(Integer var1)
+            {
+                System.out.println(var1);
+            }
+        });
+    }
+}
